61b9097137c6fe75218787f94354b0a26382ce51
thiago.novi <thiagonovi6@gmail.com>
2023-05-22 09:58:03 -0300

    Inicinei o projeto

---
+++ main.py
@@ -0,0 +1,20 @@
+import shutil
+
+def copy_file(source_path, destination_path):
+    try:
+        shutil.copy(source_path, destination_path)
+        print("File copied successfully!")
+    except FileNotFoundError:
+        print("Source file not found.")
+    except PermissionError:
+        print("Permission denied.")
+    except shutil.SameFileError:
+        print("Source and destination are the same.")
+    except:
+        print("An error occurred while copying the file.")
+
+# Example usage
+source_file = r'C:\path\to\source\file.txt'
+destination_folder = r'C:\path\to\destination\folder'
+
+copy_file(source_file, destination_folder)
